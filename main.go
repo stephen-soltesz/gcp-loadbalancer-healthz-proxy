5a0a23e4edd7805224f4a7aa75e576b8d0bb2d35
Stephen Soltesz <stephen.soltesz@gmail.com>
2018-11-03 19:53:44 -0400

    Use ReverseProxy add unit tests

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
+// gcp-service-discovery provides an HTTP-to-insecure-HTTPS proxy.
+//
+// For example, this can be helpful for Google LoadBalancer health
+// checkers which only support HTTP health-check targets.
 package main
 
 import (
 	"crypto/tls"
 	"flag"
-	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
-	"time"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/m-lab/go/rtx"
 )
 
 var (
@@ -22,50 +27,36 @@ func init() {
 		"Listen on the given address.")
 }
 
+func newReverseProxy(target *url.URL) *httputil.ReverseProxy {
+	director := func(req *http.Request) {
+		// Override the http "Host:" header to specify target.Host.
+		req.Host = target.Host
+		// Override the request scheme and host to connect via TLS.
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+	}
+
+	// Override the default transport settings to support insecure TLS connections.
+	transport := http.DefaultTransport.(*http.Transport)
+	transport.TLSClientConfig = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	return &httputil.ReverseProxy{
+		Director:  director,
+		Transport: transport,
+	}
+}
+
 func main() {
 	flag.Parse()
 
-	// A client for making all backend requests.
-	c := http.Client{
-		Transport: &http.Transport{
-			// Setup support for insecure TLS connections.
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-			// Duplicate settings from http.DefaultTransport.
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-				DualStack: true,
-			}).DialContext,
-			MaxIdleConns:          100,
-			IdleConnTimeout:       90 * time.Second,
-			TLSHandshakeTimeout:   10 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-		},
+	target, err := url.Parse(rawURL)
+	rtx.Must(err, "Failed to parse given url: %s", rawURL)
+	if target.RawQuery != "" || target.RawPath != "" {
+		log.Fatal("Do not provide target path or queries. " +
+			"All paths and queries are copied from incoming requests.")
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Issue request, and read full response.
-		resp, err := c.Get(rawURL + r.URL.Path)
-		if err != nil {
-			log.Printf("Error getting (%s%s): %v", rawURL, r.URL.Path, err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		// Report the same status and content.
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Printf("Error reading body of (%s%s): %v", rawURL, r.URL.Path, err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		// Report the same status and content.
-		// Note: we ignore headers.
-		w.WriteHeader(resp.StatusCode)
-		w.Write(body)
-	})
-
+	http.Handle("/", newReverseProxy(target))
 	log.Fatal(http.ListenAndServe(rawAddr, nil))
 }
---
+++ main_test.go
@@ -0,0 +1,90 @@
+// gcp-service-discovery provides an HTTP-to-insecure-HTTPS proxy.
+//
+// For example, this can be helpful for Google LoadBalancer health
+// checkers which only support HTTP health-check targets.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
+	"testing"
+
+	"github.com/m-lab/go/rtx"
+)
+
+func Test_newReverseProxy(t *testing.T) {
+	tests := []struct {
+		name        string
+		target      *url.URL
+		statusCode  int
+		fileContent string
+		path        string
+		want        *httputil.ReverseProxy
+		wantErr     bool
+	}{
+		{
+			name:        "success",
+			statusCode:  http.StatusOK,
+			path:        "/",
+			fileContent: "ok",
+		},
+		{
+			name:        "success-status-code-propagates",
+			statusCode:  http.StatusNotFound,
+			path:        "/",
+			fileContent: "ok",
+		},
+		{
+			name:        "success-path-propagates",
+			statusCode:  http.StatusOK,
+			path:        "/healthz",
+			fileContent: "ok",
+		},
+		{
+			name:        "success-path-with-query-parameters",
+			statusCode:  http.StatusOK,
+			path:        "/healthz?foo=bar&thing=that",
+			fileContent: "ok",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create a tls target server that the reverse proxy will query.
+			tls := httptest.NewTLSServer(
+				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(tt.statusCode)
+					fmt.Fprint(w, tt.fileContent+r.URL.String())
+				}),
+			)
+			defer tls.Close()
+
+			// Setup the reverse proxy & test server to target the tls test server.
+			u, err := url.Parse(tls.URL)
+			rtx.Must(err, "httptest URL could not be parsed: %s", tls.URL)
+			rp := newReverseProxy(u)
+			ts := httptest.NewServer(rp)
+			defer ts.Close()
+
+			// Issue a request to the second test server and verify return values.
+			resp, err := http.Get(ts.URL + tt.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("newReverseProxy request error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if resp.StatusCode != tt.statusCode {
+				t.Errorf("newReverseProxy request StatusCode; got = %v, want %v",
+					resp.StatusCode, tt.statusCode)
+			}
+			b, err := ioutil.ReadAll(resp.Body)
+			rtx.Must(err, "Failed to read response")
+			if string(b) != tt.fileContent+tt.path {
+				t.Errorf("newReverseProxy fileContent; got = %v, want %v",
+					string(b), tt.fileContent+tt.path)
+			}
+
+		})
+	}
+}
